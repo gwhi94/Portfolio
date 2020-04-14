c3ecfc2254e2a181ec990c397f94117c9ccf7318
George Whiting <gwhi94@gmail.com>
2020-04-14 20:01:08 +0100

    Initial content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio-redesign';
+  title = 'George Whiting';
+  config: any;
+  fullpage_api: any;
+
+  constructor() {
+
+    // for more details on config options please visit fullPage.js docs
+    this.config = {
+
+      // fullpage options
+      licenseKey: '877B9219-D7E245F4-AAC299F7-86C5D048',
+      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+      menu: '#menu',
+
+      // fullpage callbacks
+      afterResize: () => {
+        console.log("After resize");
+      },
+      afterLoad: (origin, destination, direction) => {
+        console.log(origin.index);
+      }
+    };
+  }
+
+  getRef(fullPageRef) {
+    this.fullpage_api = fullPageRef;
+  }
+
 }
